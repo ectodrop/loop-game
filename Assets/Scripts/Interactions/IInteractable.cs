ad84716ccdd2f33c25916032334364fdfbd5fc47
Howard Yang <yanghoward1@gmail.com>
2024-01-27 11:49:35 -0500

    add comments

--- Assets/Scripts/Interactions/IInteractable.cs
+++ Assets/Scripts/Interactions/IInteractable.cs
@@ -1,6 +1,11 @@
 public interface IInteractable
 {
+    // The Text that should be displayed at the cursor when the player hovers over the Object
     string DisplayText { get; }
+    
+    // This function will be called with the player interacts with the object
     void Interact();
+
+    // Can have the interactable object enable or disable interaction to play an animation
     bool CanInteract();
 }
