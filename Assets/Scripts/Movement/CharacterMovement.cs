4947725b5350ddacf7aae8b57a8c812701812140
emilyyyzh <emilyy.zhou@mail.utoronto.ca>
2024-02-16 22:59:29 -0500

    added walking animation

--- Assets/Scripts/Movement/CharacterMovement.cs
+++ Assets/Scripts/Movement/CharacterMovement.cs
@@ -14,6 +14,7 @@ public class CharacterMovement : MonoBehaviour
     private const float JumpHeight = 1.0f; // Adjust as needed
     private const float Gravity = -9.81f;
 
+
     // Start is called before the first frame update
     void Start()
     {
