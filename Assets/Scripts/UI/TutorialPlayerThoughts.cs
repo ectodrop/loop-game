bff667dc3c5173d917d051ab08b9f67b3a55d694
emilyyyzh <emilyy.zhou@mail.utoronto.ca>
2024-02-24 18:00:13 -0500

    Added power generator tutorial

---
+++ Assets/Scripts/UI/TutorialPlayerThoughts.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialPlayerThoughts : MonoBehaviour
+{
+    public TextMeshProUGUI playerThoughts;
+    private bool _bounce = false;
+    private int _num_bounces = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_bounce)
+        {
+            if (_num_bounces > 6)
+            {
+                playerThoughts.text = "The power generator needs to be turned off.";
+            }
+            else if (_num_bounces > 3)
+            {
+                playerThoughts.text = "Maybe the control panel will be helpful.";
+            }
+            else
+            {
+                playerThoughts.text = "Ouch. The water is electrified by the power generator.";
+            }
+
+        }
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.CompareTag("puddle"))
+        {
+            _num_bounces += 1;
+            _bounce = true;
+        }
+    }
+}
