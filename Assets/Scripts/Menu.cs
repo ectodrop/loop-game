c17f07bfe578dfa239764151ddd9f5b0e86dd656
Howard Yang <yanghoward1@gmail.com>
2024-02-27 05:58:43 -0500

    minor updates

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -5,9 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    public void OnTutorialButton()
+    {
+        SceneManager.LoadScene("Tutorial-Power");
+    }
+    public void OnTutorialButton1()
+    {
+        SceneManager.LoadScene("Tutorial-emily");
+    }
     public void OnPlayButton()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene("Level One");
     }
 
     public void OnQuitButton()
