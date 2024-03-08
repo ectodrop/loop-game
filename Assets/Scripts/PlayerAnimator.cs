2c67ff03710a91431450444ca8360d6dbf6026cc
emilyyyzh <emilyy.zhou@mail.utoronto.ca>
2024-03-07 21:08:23 -0500

    final character model animations

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -8,6 +8,10 @@ public class PlayerAnimator : MonoBehaviour
     public SoundEffect footsteps;
     // For character animation
     public Animator _playerAnimator;
+
+    private string _starting_foot;
+    private bool isWalking = false;
+
     void Start()
     {
         _playerAnimator = GetComponent<Animator>();
@@ -21,23 +25,44 @@ public class PlayerAnimator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        if (!isWalking)
         {
-            _playerAnimator.SetBool("isWalking", true);
+            if (pressedWalking())
+            {
+                if (Random.Range(0f, 1.0f) < 0.5f)
+                {
+                    _starting_foot = "isWalking";
+                }
+                else
+                {
+                    _starting_foot = "isWalkingRight";
+                }
+                _playerAnimator.SetBool(_starting_foot, true);
 
-            if (Input.GetKey(KeyCode.LeftShift))
-            {
-                _playerAnimator.SetFloat("walkingSpeed", 2f);
-            } else
-            {
-                _playerAnimator.SetFloat("walkingSpeed", 1f);
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    _playerAnimator.SetFloat("walkingSpeed", 2f);
+                }
+                else
+                {
+                    _playerAnimator.SetFloat("walkingSpeed", 1f);
+                }
+                isWalking = true;
             }
-            
-        }
-        else
+        } else
         {
-            _playerAnimator.SetFloat("walkingSpeed", 0f);
-            _playerAnimator.SetBool("isWalking", false);
+            if (!pressedWalking())
+            {
+                _playerAnimator.SetFloat("walkingSpeed", 0f);
+                _playerAnimator.SetBool(_starting_foot, false);
+                isWalking = false;
+            }
         }
+        
+    }
+
+    bool pressedWalking()
+    {
+        return Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D);
     }
 }
